ddd42cd7b9886089cb29f5a469bb22726efa00f7
Nimander <szymanskimichal57@gmail.com>
2018-04-05 14:47:06 +0200

    the best book will be picked at the end

--- ksiegarnia.pl
+++ ksiegarnia.pl
@@ -2,6 +2,7 @@
 :- dynamic nielubi/1.
 :- dynamic punkty/2.
 :- dynamic koniec/1.
+:- dynamic wybrana/2.
 :-(clause(cechy(_),_); consult('baza.pl')).  
 
 start:-
@@ -15,14 +16,24 @@ start:-
 start:-
 	funkcja_init.
 start:-
-	funkcja_licznik.
+	funkcja_licznik1.
+start:-
+	funkcja_licznik2.
 start:-
 	nl,funkcja_wypisz_pkt.
+start:-
+	funkcja_init_2.
+start:-
+	nl,funkcja.
+start:-
+	nl,funkcja2.
 
+
+	
 jaka_ksiazka:-
 	cechy(X),					%sprawdza cechy 
 	not(koniec(tak)),			%jezeli koniec to nie pojdzie dalej
-	write('czy lubisz t/n/k '),
+	write('czy lubisz t/n/o/k '),
 	write(X),nl,
 	read(Odp),
 	decyzja(Odp, X),
@@ -34,7 +45,7 @@ decyzja('t', X):-
 decyzja('n', X):-
 	assertz(nielubi(X)).
 	
-decyzja('k', X):-
+decyzja('k', _):-
 	assertz(koniec(tak)).
 	
 wypisz_co_lubi:-
@@ -56,14 +67,23 @@ wypisz_co_nie_lubi:-
 funkcja_init:-
 	cecha(Tytul, X),
 	not(punkty(Tytul, _)),
-	assertz(punkty(Tytul, 0)),
+	assertz(punkty(Tytul, 4)),
 	fail.
 	
-funkcja_licznik:-
+funkcja_licznik1:-
 	cecha(Tytul, X),
 	lubi(X),
 	punkty(Tytul, Pkt),
 	Pkt_nowe is Pkt + 1,
+	retract(punkty(Tytul, Pkt)),
+	assertz(punkty(Tytul, Pkt_nowe)),
+	fail.
+	
+funkcja_licznik2:-
+	cecha(Tytul, X),
+	nielubi(X),
+	punkty(Tytul, Pkt),
+	Pkt_nowe is Pkt - 1,
 	retract(punkty(Tytul, Pkt)),
 	assertz(punkty(Tytul, Pkt_nowe)),
 	fail.
@@ -73,9 +93,30 @@ funkcja_wypisz_pkt:-
 	write(Tytul), write(' '), write(Pkt), nl,
 	fail.
 	
+funkcja_init_2:-
+	punkty(wiedzmin, Pkt),
+	assertz(wybrana(wiedzmin, Pkt)),
+	fail.
+
+funkcja:-
+	punkty(Tytul, Pkt),
+	wybrana(X, P),
+	Pkt > P,
+	retract(wybrana(X, P)),
+	assertz(wybrana(Tytul, Pkt)),
+	fail.
 	
 	
+funkcja2:-
+	wybrana(X, Y),
+	write('Powinieneœ przeczytaæ '),
+	write(X), write(', ksi¹¿ka ta zdoby³a '), write(Y),
+	Procent is (Y*100)/8,
+	write(' punktów. Na '), write(Procent), write('% Ci siê spodoba!'),
+	nl,nl, halt.
 	
+	
+	% swipl --goal=start --stand_alone=true -o ksiegarnia.exe -c ksiegarnia.pl
 	
 	
 	
